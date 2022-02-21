0a39969d99a1fc13172693a54075b4df622b5119
Matias Rietig <matias.rietig@corp.meraki.com>
2022-02-21 20:30:25 +0100

    latest working version

--- src/mapping.ts
+++ src/mapping.ts
@@ -2,7 +2,8 @@ import { SummonComplete } from '../generated/MolochSummoner/MolochSummoner'
 import { Dao } from '../generated/schema'
 
 export function handleNewDAO(event: SummonComplete): void {
-  let dao = new Dao('_' + Math.random().toString(36).substr(2, 9))
+  let dao = new Dao(event.params.moloch.toHexString())
+  event.params.
   dao.gracePeriod = event.params.gracePeriodLength
   dao.dilutionBond = event.params.dilutionBound
   dao.save()
